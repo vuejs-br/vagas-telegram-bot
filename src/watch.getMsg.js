4a6e86e731ca3baf763578a680beedbf9735309c
Jeudi Prando Araujo <jeudiprando@gmail.com>
2020-11-26 15:02:50 -0300

    Update watch.getMsg.js
    keywords para remover das mensagens da issue/vaga

--- src/watch.getMsg.js
+++ src/watch.getMsg.js
@@ -1,16 +1,21 @@
 const NOBODY = 'POR FAVOR, SÓ POSTE SE A VAGA FOR PARA FRONT-END!'
 
 const getMsg = ({body, title, labels, html_url}) =>  {
-  [
-    '## Nossa empresa', '## Nossa Empresa',
-    'Nossa empresa', 'Nossa Empresa', '## Empresa',
-    '## Descrição da vaga', '## Local', '## Requisitos',
-    '## Contratação', '## Como se candidatar', '## Labels',
-    '#### Alocação', '#### Regime', '#### Nível',
-    '**', '##', '###', '####',
+  body = body.replace(/(([\t ]*nossa|[#]*[\t ]*[#]+[\t ]*(nossa)?)[\t ]*empresa[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*n[íi]vel[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*regime[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*aloca[çc][ãa]o[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*local[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*labels[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*requisito[s]?[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*contrata[çc][ãa]o[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*descri[çc][ãa]o da vaga[\t ]*)/gmi,' ')
+  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*como se candidatar[\t ]*)/gmi,' ')
+
+  [ '**', '##', '###', '####',
     '> Vaga Remota durante a pandemia'
   ].forEach(w => { body = body.split(w).join(' ') });
-
+  
   [' ', '\r\n', '\r','\n'].forEach(c => {
     body = body.split(c)
     .map(w => w.trim())
