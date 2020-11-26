d1ffc429e56e53a6ea23a3881698e34c01529d58
Jeudi Prando Araujo <jeudiprando@gmail.com>
2020-11-26 15:12:02 -0300

    Update watch.getMsg.js
    refatoracao, keywords para remover do corpo da mensagem da issue/vaga

--- src/watch.getMsg.js
+++ src/watch.getMsg.js
@@ -1,17 +1,24 @@
 const NOBODY = 'POR FAVOR, SÓ POSTE SE A VAGA FOR PARA FRONT-END!'
 
-const getMsg = ({body, title, labels, html_url}) =>  {
-  body = body.replace(/(([\t ]*nossa|[#]*[\t ]*[#]+[\t ]*(nossa)?)[\t ]*empresa[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*n[íi]vel[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*regime[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*aloca[çc][ãa]o[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*local[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*labels[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*requisito[s]?[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*contrata[çc][ãa]o[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*descri[çc][ãa]o da vaga[\t ]*)/gmi,' ')
-  body = body.replace(/([\t ]*[#]*[\t ]*[#]+[\t ]*como se candidatar[\t ]*)/gmi,' ')
+const regekiz = [
+    /(([\t ]*nossa|[#]*[\t ]*[#]+[\t ]*(nossa)?)[\t ]*empresa[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*n[íi]vel[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*regime[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*aloca[çc][ãa]o[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*local[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*label[s]?[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*requisito[s]?[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*contrata[çc][ãa]o[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*descri[çc][ãa]o da vaga[\t ]*)/gmi,
+    /([\t ]*[#]*[\t ]*[#]+[\t ]*como se candidatar[\t ]*)/gmi
+]
 
+const getMsg = ({body, title, labels, html_url}) =>  {
+  body = regekiz.reduce((txt, rgx) => {
+    txt = txt.replace(rgx, ' ')
+    return txt
+  }, body)
+  
   [ '**', '##', '###', '####',
     '> Vaga Remota durante a pandemia'
   ].forEach(w => { body = body.split(w).join(' ') });
