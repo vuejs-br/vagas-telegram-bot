ba8ee8e66ce8a7126a7f5b49804aee996f8c6978
Jeudi Prando Araujo <jeudiprando@gmail.com>
2020-11-26 15:26:31 -0300

    Update watch.getMsg.js
    refactory

--- src/watch.getMsg.js
+++ src/watch.getMsg.js
@@ -1,16 +1,13 @@
 const NOBODY = 'POR FAVOR, SÓ POSTE SE A VAGA FOR PARA FRONT-END!'
 
+const removeWords = [
+    'regime',  'local', 'como se candidatar',
+    'n[íi]vel', 'aloca[çc][ãa]o', 'label[s]?', 'requisito[s]?', 'contrata[çc][ãa]o', 'descri[çc][ãa]o da vaga'    
+]
+
 const regekiz = [
     /(([\t ]*nossa|[#]*[\t ]*[#]+[\t ]*(nossa)?)[\t ]*empresa[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*n[íi]vel[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*regime[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*aloca[çc][ãa]o[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*local[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*label[s]?[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*requisito[s]?[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*contrata[çc][ãa]o[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*descri[çc][ãa]o da vaga[\t ]*)/gmi,
-    /([\t ]*[#]*[\t ]*[#]+[\t ]*como se candidatar[\t ]*)/gmi
+    ...[removeWords].map(v => RegExp(`/([\t ]*[#]*[\t ]*[#]+[\t ]*${v}[\t ]*)/gmi`))
 ]
 
 const getMsg = ({body, title, labels, html_url}) =>  {
